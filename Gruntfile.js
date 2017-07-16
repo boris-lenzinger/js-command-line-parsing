b1e6d6fb389cd28a89118f17458ecbbe6be46d08
boris <boris.lenzinger@gmail.com>
2017-07-16 20:39:44 +0200

    Adding support for code coverage.
    Adding configuration for Grunt based on the Grunt documentation
    https://gruntjs.com/
    
    and the documentation of the code coverage :
    https://github.com/jribble/grunt-jasmine-node-coverage
    
    Adding also the code for testing the library. The spec file for the library
    is using the jasmine-node syntax. Because jasmine-node supports only jasmine
    less than 2.X, there is no support for beforeAll function.

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%=grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		},
+		jasmine_node: {
+			task_name: {
+				options: {
+					forceExit: true,
+					coverage: {
+						reportFile: 'coverage.json',
+						relativize: true,
+						thresholds: {
+							statements: 0,
+							branches: 0,
+							lines: 0,
+							functions: 0
+						},
+						watermarks: {
+							statements: [50, 80],
+							lines: [50, 80],
+							functions: [50, 80],
+							branches: [50, 80]
+						},
+						includeAllSources: true,
+						reportDir: 'coverage',
+						report: [
+							'lcov',
+							'text-summary'
+						],
+						collect: [
+							'*coverage.json'
+						],
+						excludes: []
+					},
+					jasmine: {
+						spec_dir: 'spec',
+						spec_files: [
+							'**/*[sS]pec.js'
+						],
+						reporters: {
+							savePath: "reports",
+							consolidateAll: true
+						}
+					}
+				},
+				src: ['src/**/*.js']
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify', 'jasmine_node']);
+
+};
---
+++ spec/lib-parsing-spec.js
@@ -0,0 +1,88 @@
+var parser = require('../src/lib-parsing.js');
+
+
+describe("Parsing of a parameters file", function() {
+
+    it("Checking that the definition of parameters is loaded.", function() {
+	parser.defineParameters("src/parameters.json");
+	expect(parser.libraryAvailableParameters).toBeDefined();
+    });
+    
+    
+    it("Checking that the content of the parameters definition is ok.", function() {
+	parser.defineParameters("src/parameters.json");
+	expect(parser.libraryAvailableParameters['filepath']).toBeDefined();
+	expect(parser.libraryAvailableParameters['tokenName']).toBeDefined();
+	expect(parser.libraryAvailableParameters['tokenValue']).toBeDefined();
+	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly']).toBeDefined();
+    });
+
+    it("Checking that the content of the filepath definition is ok.", function() {
+	parser.defineParameters("src/parameters.json");
+	expect(parser.libraryAvailableParameters['filepath'].doc).toBeDefined();
+	expect(parser.libraryAvailableParameters['filepath'].longOption).toBeDefined();
+	expect(parser.libraryAvailableParameters['filepath'].longOption).toEqual('--path-to-file');
+	
+    });
+
+    it("Checking that the content of the tokenName definition is ok.", function() {
+	parser.defineParameters("src/parameters.json");
+	expect(parser.libraryAvailableParameters['tokenName'].doc).toBeDefined();
+	expect(parser.libraryAvailableParameters['tokenName'].longOption).toBeDefined();
+	expect(parser.libraryAvailableParameters['tokenName'].longOption).toEqual('--token');
+	
+    });
+
+    it("Checking that the content of the tokenValue definition is ok.", function() {
+	parser.defineParameters("src/parameters.json");
+	expect(parser.libraryAvailableParameters['tokenValue'].doc).toBeDefined();
+	expect(parser.libraryAvailableParameters['tokenValue'].longOption).toBeDefined();
+	expect(parser.libraryAvailableParameters['tokenValue'].longOption).toEqual('--value');
+	
+    });
+
+    it("Checking that the content of the flagReplaceFirstOnly definition is ok.", function() {
+	parser.defineParameters("src/parameters.json");
+	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly'].doc).toBeDefined();
+	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly'].longOption).toBeDefined();
+	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly'].value).toBeDefined();
+	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly'].value).toBe(false);
+	
+    });
+
+});
+
+
+describe('Checking that parsing is defining the good values', function() {
+    var parameters;
+ 
+    beforeEach(function() {
+	parameters = parser.parse(['--path-to-file', '/tmp/tokenized-file', 'isolatedValue', '--token', 'name', '--value', 'replacement', '--replace-first-only', 'anotherIsolatedValue']);
+    });
+
+    it("Passing a valid command line", function() {
+	expect(parameters).toBeDefined();
+	expect(parameters['filepath']).toBe('/tmp/tokenized-file');
+	expect(parameters['tokenName']).toBe('name');
+	expect(parameters['tokenValue']).toBe('replacement');
+	expect(parameters['flagReplaceFirstOnly']).toBe(true);
+	expect(parameters['_unassigned']).toBeDefined();
+	expect(parameters['_unassigned'].length).toBe(2);
+	expect(parameters['_unassigned'][0]).toBe('isolatedValue');
+	expect(parameters['_unassigned'][1]).toBe('anotherIsolatedValue');
+    });
+});
+
+
+// ========================= ERROR  CASES ===================
+describe('Error cases must be well handled', function() {
+    var foo;
+
+    beforeAll(function() {
+	foo = 1;
+    });
+
+    it('Passing unknown option must throw an exception', function() {
+	
+    });
+});
