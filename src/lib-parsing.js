708f53f9131f8d1c39d87e77124f764fdbce927a
boris <boris.lenzinger@gmail.com>
2017-07-16 23:55:59 +0200

    Fixing documentation of functions.
    Improving the documentation of the code. It was not relevant and was false in
    some cases. This should be ok now.

--- src/lib-parsing.js
+++ src/lib-parsing.js
@@ -4,11 +4,25 @@ var exports = module.exports={};
 
 exports.libraryAvailableParameters = {};
 
+/**
+ * Function to ingest the file defining the parameters for the parsing.
+ * The parsing is done, by default with the parameters that are supplied
+ * to this function.
+ */
 exports.defineParameters = function(pathToParametersFile) {
     exports.libraryAvailableParameters = JSON.parse(fs.readFileSync(pathToParametersFile, 'utf8'));
 }
 
-// Now we can parse to build a parameter object.
+
+/**
+ * Parses the command line passed as parameter. The function uses, by default the parameters
+ * passed to the function 'defineParameters'. But you can pass your own set of parameters
+ * and they will be used to make the parsing.
+ *
+ * @return : the function returns a parameter object where the field name is the parameter
+ * name as specified in the object availableParameters. The value stored for the field is the
+ * value parsed by the function.
+ */
 exports.parse = function(arguments, availableParameters) {
     if ( availableParameters == undefined ) {
 	availableParameters = exports.libraryAvailableParameters;
@@ -46,7 +60,12 @@ exports.parse = function(arguments, availableParameters) {
 }
 
 
-
+/**
+ * This function finds the definition of a parameter in the list of the
+ * available parameters.
+ * This function is used in the parsing function. It is currently exposed
+ * but it might changed and not be anymore exported in a near future.
+ */
 exports.getOptionDefinition = function(availableParameters, parameter) {
     var definition = {};
     for ( var candidate in availableParameters ) {
