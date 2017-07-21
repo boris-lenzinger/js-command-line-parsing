e41e68c21adc66ddb1d1b578380d19626ce1bf67
boris <boris.lenzinger@gmail.com>
2017-07-21 09:14:10 +0200

    Moving initialization phasis to methode beforeEach.

--- spec/lib-parsing-spec.js
+++ spec/lib-parsing-spec.js
@@ -3,14 +3,16 @@ var parser = require('../src/lib-parsing.js');
 
 describe("Parsing of a parameters file", function() {
 
-    it("Checking that the definition of parameters is loaded.", function() {
+    beforeEach(function() {
 	parser.defineGlobalParameters("src/parameters.json");
+    });
+
+    it("Checking that the definition of parameters is loaded.", function() {
 	expect(parser.libraryAvailableParameters).toBeDefined();
     });
     
     
     it("Checking that the content of the parameters definition is ok.", function() {
-	parser.defineGlobalParameters("src/parameters.json");
 	expect(parser.libraryAvailableParameters['filepath']).toBeDefined();
 	expect(parser.libraryAvailableParameters['tokenName']).toBeDefined();
 	expect(parser.libraryAvailableParameters['tokenValue']).toBeDefined();
@@ -18,7 +20,6 @@ describe("Parsing of a parameters file", function() {
     });
 
     it("Checking that the content of the filepath definition is ok.", function() {
-	parser.defineGlobalParameters("src/parameters.json");
 	expect(parser.libraryAvailableParameters['filepath'].doc).toBeDefined();
 	expect(parser.libraryAvailableParameters['filepath'].longOption).toBeDefined();
 	expect(parser.libraryAvailableParameters['filepath'].longOption).toEqual('--path-to-file');
@@ -26,7 +27,6 @@ describe("Parsing of a parameters file", function() {
     });
 
     it("Checking that the content of the tokenName definition is ok.", function() {
-	parser.defineGlobalParameters("src/parameters.json");
 	expect(parser.libraryAvailableParameters['tokenName'].doc).toBeDefined();
 	expect(parser.libraryAvailableParameters['tokenName'].longOption).toBeDefined();
 	expect(parser.libraryAvailableParameters['tokenName'].longOption).toEqual('--token');
@@ -34,7 +34,6 @@ describe("Parsing of a parameters file", function() {
     });
 
     it("Checking that the content of the tokenValue definition is ok.", function() {
-	parser.defineGlobalParameters("src/parameters.json");
 	expect(parser.libraryAvailableParameters['tokenValue'].doc).toBeDefined();
 	expect(parser.libraryAvailableParameters['tokenValue'].longOption).toBeDefined();
 	expect(parser.libraryAvailableParameters['tokenValue'].longOption).toEqual('--value');
@@ -42,7 +41,6 @@ describe("Parsing of a parameters file", function() {
     });
 
     it("Checking that the content of the flagReplaceFirstOnly definition is ok.", function() {
-	parser.defineGlobalParameters("src/parameters.json");
 	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly'].doc).toBeDefined();
 	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly'].longOption).toBeDefined();
 	expect(parser.libraryAvailableParameters['flagReplaceFirstOnly'].value).toBeDefined();
@@ -80,7 +78,6 @@ describe('Checking parameters picking...', function() {
  
     beforeEach(function() {
 	
-	parameters = parser.parse(['--path-to-file', '/tmp/tokenized-file', 'isolatedValue', '--token', 'name', '--value', 'replacement', '--replace-first-only', 'anotherIsolatedValue']);
     });
     
     it('Picking a parameter', function() {
